ea39d52617f56b5ec3ab092aca4938a383137a6f
Adam Wolfman <adamwolfman@Adams-MBP.domain>
2019-07-19 14:18:57 -0600

    updated homepage

---
+++ build/precache-manifest.6990e55445ff356e0d23dc03b62153ee.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19e2866fd230ccd62eddd4388609828f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8cea61678cb7249d4d54",
+    "url": "/static/css/2.48adc591.chunk.css"
+  },
+  {
+    "revision": "8067cf34fcbf37fe54c4",
+    "url": "/static/css/main.dbfe448b.chunk.css"
+  },
+  {
+    "revision": "8cea61678cb7249d4d54",
+    "url": "/static/js/2.69070adf.chunk.js"
+  },
+  {
+    "revision": "8067cf34fcbf37fe54c4",
+    "url": "/static/js/main.c712806b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f5914be30da38dc609bb5eae048f1d79",
+    "url": "/static/media/barrels.f5914be3.jpg"
+  },
+  {
+    "revision": "7a86ad6040547caa6b8990fe5e01362a",
+    "url": "/static/media/contact-roxy-logo.7a86ad60.png"
+  },
+  {
+    "revision": "ab61c6241b7b69749be4b3c4639abea4",
+    "url": "/static/media/gramophone_ivory.ab61c624.jpg"
+  },
+  {
+    "revision": "cf59c5b73d5ef38e67320ebcfc5c1d56",
+    "url": "/static/media/gramophone_logo_white.cf59c5b7.png"
+  },
+  {
+    "revision": "798d901296cf3600998e0722c48497f9",
+    "url": "/static/media/menu-roxy-logo.798d9012.png"
+  },
+  {
+    "revision": "05a36bd76737cd63153f43a400785cbf",
+    "url": "/static/media/openformusic.05a36bd7.png"
+  },
+  {
+    "revision": "5a8061f966c31973911f7cc3dc1fa9f8",
+    "url": "/static/media/roxy_giftcard.5a8061f9.jpg"
+  },
+  {
+    "revision": "be7f2d48480700755805d49b111e0459",
+    "url": "/static/media/shop_roxy.be7f2d48.png"
+  },
+  {
+    "revision": "8b664c63757dc1366aa9e0a438547af2",
+    "url": "/static/media/wood_texture.8b664c63.jpg"
+  }
+]);
