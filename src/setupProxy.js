b0d5a9ec3e3682a192eb3933798e16cdb23c8291
Adam Wolfman <adamwolfman@Adams-MBP.domain>
2019-07-19 16:06:54 -0600

    updated netlify stuff

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const proxy = require('http-proxy-middleware');
 module.exports = function(app) {
   app.use(
     proxy('/.netlify/functions/', {
-      target: 'http://localhost:9000/',
+      target: '/.netlify/functions/',
       pathRewrite: {
         '^/\\.netlify/functions': '',
       },
