a7e07b864ff4e55a16a5da18be151debee4c2d63
Adam Wolfman <adamwolfman@Adams-MBP-2.domain>
2021-01-30 13:08:50 -0700

    updated insta access

--- src/InstagramFeed/InstagramFeed.jsx
+++ src/InstagramFeed/InstagramFeed.jsx
@@ -19,7 +19,7 @@ class InstagramFeed extends Component {
     }
 
     getPosts = async () => {
-        const apiKey = 'IGQVJXcEpiTUFpdndGNFVjQk5lSUtvamw4bHhndlAyNlJjSGpTSlVIT19GTW5hRkRvSld0S1dFLVRadks2TEEySmNpY1NXNEpweVNNQndSYll6MldoeHlHN3JEVFNRUTlvb0NtQ19n';
+        const apiKey = 'IGQVJWbjR5SnBBUVJuelVOczBLTHYtNEtfV200bm9CUUliOTFCMDNVUXVMaXRNTEV5RXptRnE4ekU0ZADhRZAG5wdm9mWHJpbjV5dXhUcU1QQnoxbWVrbW5xRXFyWE1WLXF0S3JqbWR0ZA1lwN1N1UkN0awZDZD';
         
         try {
           const response = await fetch(`https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,thumbnail_url,permalink&access_token=${apiKey}`);
